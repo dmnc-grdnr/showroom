9c917f5e446c27a1ddd0eb5041b718f4645d7fa8
dmnc-grdnr <spamadesso@gmail.com>
2025-01-30 16:31:10 +0100

    initial commit for new project DFS

---
+++ DFS/README.md
@@ -0,0 +1,19 @@
+# [DFS] 
+## Dockerized FHIR-server with resources imported from a simple FHIR-repository
+
+### Start-Up
+
+`docker compose up`
+
+### Check patient resources
+
+`https://localhost:9090/fhir/Patient`
+
+### Shut-Down
+
+`docker compose down`
+
+`docker volume prune`
+
+### TODO:
+* use https://pypi.org/project/fhir.resources/ to create resources instead of simply copying data
---
+++ DFS/python/fill_json.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+import requests
+import json
+
+
+fhir_api = "http://BLAZE_SRC_APP:8080/fhir"
+
+if __name__ == "__main__":
+    count_success = 0
+    for item in Path('./data/output/fhir').iterdir():
+        if item.is_file():
+
+            with open(item, 'r') as resource_file:
+                resource = json.load(resource_file)
+
+            response = requests.post(fhir_api, json=resource)
+            if response.status_code == 200:
+                count_success += 1
+    print(f"{count_success} resource(s) were successfully inserted.")
